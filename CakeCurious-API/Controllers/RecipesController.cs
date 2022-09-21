4b7be2eb3e9908775c5e1db9e9ae80e19b9f6235
ngianguyen81 <ngianguyen81@gmail.com>
2022-09-21 18:52:34 +0700

    endpoint: get follow recipes

---
+++ CakeCurious-API/Controllers/RecipesController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessObject;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Interfaces;
+using Repository.Models.Recipes;
+using System.Security.Claims;
+
+namespace CakeCurious_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly IRecipeRepository recipeRepository;
+
+        public RecipesController(IRecipeRepository _recipeRepository)
+        {
+            recipeRepository = _recipeRepository;
+        }
+
+        [HttpGet("following")]
+        [Authorize]
+        public ActionResult<ICollection<FollowRecipe>> GetRecipesFromFollowing(int skip = 0, int take = 5)
+        {
+            // Get ID Token
+            string? uid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!string.IsNullOrWhiteSpace(uid))
+            {
+                return Ok(recipeRepository.GetLatestRecipesForFollower(uid, skip, take));
+            }
+            return Unauthorized();
+        }
+    }
+}
