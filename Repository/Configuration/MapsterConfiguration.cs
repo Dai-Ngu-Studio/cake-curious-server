d8b49dffeca6a5d13ee5a7e57c0c88097c8817e6
ngianguyen81 <ngianguyen81@gmail.com>
2022-09-24 18:52:02 +0700

    re-organize mapping

--- Repository/Configuration/MapsterConfiguration.cs
+++ Repository/Configuration/MapsterConfiguration.cs
@@ -1,9 +1,6 @@
-﻿using BusinessObject;
-using Mapster;
+﻿using Mapster;
 using Microsoft.Extensions.DependencyInjection;
-using Repository.Constants.RecipeMaterials;
-using Repository.Constants.RecipeMedia;
-using Repository.Models.Recipes;
+using Repository.Configuration.Mappings;
 using System.Reflection;
 
 namespace Repository.Configuration
@@ -12,21 +9,8 @@ namespace Repository.Configuration
     {
         public static void RegisterMapsterConfiguration(this IServiceCollection services)
         {
-            TypeAdapterConfig<Recipe, HomeRecipe>
-                .NewConfig()
-                .Map(dest => dest.Likes, src => src.Likes!.Count);
-
-            TypeAdapterConfig<Recipe, DetailRecipe>
-                .NewConfig()
-                .Map(dest => dest.Likes, src => src.Likes!.Count)
-                .Map(dest => dest.Ingredients, src => src.RecipeMaterials!.Where(x => x.MaterialType == (int)RecipeMaterialTypeEnum.Ingredient))
-                .Map(dest => dest.Equipment, src => src.RecipeMaterials!.Where(x => x.MaterialType == (int)RecipeMaterialTypeEnum.Equipment))
-                .Map(dest => dest.RecipeSteps, src => src.RecipeSteps!.OrderBy(x => x.StepNumber))
-                .Map(dest => dest.VideoUrl, src => src.RecipeMedia!
-                    .FirstOrDefault(x => 
-                    x.MediaType == (int)RecipeMediaTypeEnum.Video) != null
-                    ? src.RecipeMedia!.FirstOrDefault(x => x.MediaType == (int)RecipeMediaTypeEnum.Video)!.MediaUrl
-                    : null);
+            RecipeMapConfiguration.RegisterRecipeMapping();
+            RecipeStepMapConfiguration.RegisterRecipeStepMapping();
 
             TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
         }
