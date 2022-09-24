bc05f4c2d3f98892a4a4369a1f1fadd5719444a5
ngianguyen81 <ngianguyen81@gmail.com>
2022-09-25 00:36:14 +0700

    endpoint to view comments of recipe

---
+++ Repository/CommentRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject;
+using Mapster;
+using Repository.Interfaces;
+using Repository.Models.Comments;
+
+namespace Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        public ICollection<RecipeComment> GetCommentsForRecipe(Guid recipeId)
+        {
+            var db = new CakeCuriousDbContext();
+            return db.Comments
+                .Where(x => x.RecipeId == recipeId && x.Depth == 0)
+                .ProjectToType<RecipeComment>()
+                .ToList();
+        }
+    }
+}
---
+++ Repository/Interfaces/ICommentRepository.cs
@@ -0,0 +1,9 @@
+﻿using Repository.Models.Comments;
+
+namespace Repository.Interfaces
+{
+    public interface ICommentRepository
+    {
+        public ICollection<RecipeComment> GetCommentsForRecipe(Guid recipeId);
+    }
+}
