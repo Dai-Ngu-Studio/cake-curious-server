2fc9af7a2ff0438dd0a03887a4c0d67bdd71168f
phucts <hooquangphuc.t4@gmail.com>
2022-10-10 12:12:20 +0700

    update file IOrderRepository.cs

--- Repository/Interfaces/IOrderRepository.cs
+++ Repository/Interfaces/IOrderRepository.cs
@@ -10,9 +10,10 @@ namespace Repository.Interfaces
 {
     public interface IOrderRepository
     {
-        public IEnumerable<StoreDashboardOrder>? GetOrders(string? s,string? order_by,string? filter_Order,int pageSize, int pageIndex);
+        public IEnumerable<StoreDashboardOrder>? GetOrdersOfAStore(string uid, string? s, string? order_by, string? filter_Order, int pageSize, int pageIndex);
         public Task UpdateOrder(Order order);
         public Task<Order?> GetById(Guid guid);
-        public int CountDashboardOrders(string? s,string? order_by,string? filter_Order);
+        public Task<StoreDashboardOrderDetail?> GetOrderDetailForStore(Guid guid);
+        public int CountDashboardOrders(string uid, string? s, string? order_by, string? filter_Order);
     }
 }
