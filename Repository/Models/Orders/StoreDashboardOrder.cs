4a9b0ad44f7766183c0dbe8c744389311c1ab750
phucts <hooquangphuc.t4@gmail.com>
2022-10-12 16:48:09 +0700

    add address prop to StoreDashboardOrder model

--- Repository/Models/Orders/StoreDashboardOrder.cs
+++ Repository/Models/Orders/StoreDashboardOrder.cs
@@ -8,6 +8,7 @@ namespace Repository.Models.Orders
         public Guid? Id { get; set; }
         public SimpleUser? User { get; set; }
         public IEnumerable<SimpleOrderDetail>? OrderDetails { get; set; }
+        public string? Address { get; set; }
         public decimal? DiscountedTotal { get; set; }
         public int? Status { get; set; }
         public DateTime? OrderDate { get; set; }      
