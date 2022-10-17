6a86f946d1022a0b7982880860d46d1ebe7af13c
phucts <hooquangphuc.t4@gmail.com>
2022-10-17 14:15:35 +0700

    add get /product-categories endpoint

---
+++ Repository/Interfaces/IProductCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IProductCategoryRepository
+    {
+        public Task<IEnumerable<ProductCategory>> GetProductCategories();
+    }
+}
