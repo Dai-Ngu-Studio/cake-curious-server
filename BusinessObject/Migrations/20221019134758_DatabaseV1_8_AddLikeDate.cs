c53673cbaf1108dfec696ac02585dcdc2ad684ed
ngianguyen81 <ngianguyen81@gmail.com>
2022-10-19 20:51:01 +0700

    like: add created_date

---
+++ BusinessObject/Migrations/20221019134758_DatabaseV1_8_AddLikeDate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObject.Migrations
+{
+    public partial class DatabaseV1_8_AddLikeDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_date",
+                table: "Like",
+                type: "datetime2(7)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_date",
+                table: "Like");
+        }
+    }
+}
