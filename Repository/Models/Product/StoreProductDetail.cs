2334a24c50054aa933e15a21d45aa56409857dec
phucts <hooquangphuc.t4@gmail.com>
2022-10-19 15:42:30 +0700

    add productCategoryId prop to StoreProductDetail model

--- Repository/Models/Product/StoreProductDetail.cs
+++ Repository/Models/Product/StoreProductDetail.cs
@@ -16,6 +16,8 @@ namespace Repository.Models.Product
 
         public SimpleProductCategory? ProductCategory { get; set; }
 
+        public int? ProductCategoryId { get; set; }
+
         public string? Name { get; set; }
 
         public string? Description { get; set; }
