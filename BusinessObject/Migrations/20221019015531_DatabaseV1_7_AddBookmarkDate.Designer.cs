e25af5a47cf66a9284bfa352b5eab553e2c6ad94
ngianguyen81 <ngianguyen81@gmail.com>
2022-10-19 09:00:02 +0700

    bookmark: add created_date

--- BusinessObject/Migrations/20221019015531_DatabaseV1_7_AddBookmarkDate.Designer.cs
+++ BusinessObject/Migrations/20221019015531_DatabaseV1_7_AddBookmarkDate.Designer.cs
@@ -4,6 +4,7 @@ using BusinessObject;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BusinessObject.Migrations
 {
     [DbContext(typeof(CakeCuriousDbContext))]
-    partial class CakeCuriousDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221019015531_DatabaseV1_7_AddBookmarkDate")]
+    partial class DatabaseV1_7_AddBookmarkDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BusinessObject/Migrations/20221019015531_DatabaseV1_7_AddBookmarkDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObject.Migrations
+{
+    public partial class DatabaseV1_7_AddBookmarkDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_date",
+                table: "Bookmark",
+                type: "datetime2(7)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_date",
+                table: "Bookmark");
+        }
+    }
+}
