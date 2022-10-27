879d0d8d2c644ddb946922dd16daa38a4363eeb9
phucts <hooquangphuc.t4@gmail.com>
2022-10-27 15:17:40 +0700

    rename getProductbyId t o getReportById

--- CakeCurious-API/Controllers/ReportsController.cs
+++ CakeCurious-API/Controllers/ReportsController.cs
@@ -44,7 +44,7 @@ namespace CakeCurious_API.Controllers
 
         [HttpGet("{guid}")]
         [Authorize]
-        public async Task<ActionResult<Product>> GetProductsById(Guid guid)
+        public async Task<ActionResult<Product>> GetReporsById(Guid guid)
         {
             var result = await _ReportRepository.GetById(guid);
             return Ok(result);
