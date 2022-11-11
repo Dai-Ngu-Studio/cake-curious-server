37c24088c8281b167b732f23a487bd779ae3c48e
ngianguyen81 <ngianguyen81@gmail.com>
2022-11-11 20:56:52 +0700

    elasticsearch models for store, user

---
+++ Repository/Models/Users/ElasticsearchUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Repository.Models.Users
+{
+    public class ElasticsearchUser
+    {
+        public string? Id { get; set; }
+        public string? Username { get; set; }
+        public string? DisplayName { get; set; }
+        public int[]? Roles { get; set; }
+    }
+}
