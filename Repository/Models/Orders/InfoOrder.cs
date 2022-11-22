51358889b5eaedb63409c416bdcf84199efd24d1
ngianguyen81 <ngianguyen81@gmail.com>
2022-11-22 14:32:56 +0700

    add business models for view orders

---
+++ Repository/Models/Orders/InfoOrder.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Models.Stores;
+
+namespace Repository.Models.Orders
+{
+    public class InfoOrder
+    {
+        public Guid? Id { get; set; }
+        public GroceryStore? Store { get; set; }
+        public decimal? Total { get; set; }
+        public decimal? DiscountedTotal { get; set; }
+        public int? Products { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? ProcessedDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public int? Status { get; set; }
+    }
+}
---
+++ Repository/Models/Orders/InfoOrderPage.cs
@@ -0,0 +1,8 @@
+﻿namespace Repository.Models.Orders
+{
+    public class InfoOrderPage
+    {
+        public int? TotalPages { get; set; }
+        public IEnumerable<InfoOrder>? Orders { get; set; }
+    }
+}
