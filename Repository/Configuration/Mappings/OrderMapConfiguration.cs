f5c037c86e1eb4337c50cfb0157241399fb8bfca
ngianguyen81 <ngianguyen81@gmail.com>
2022-11-22 14:33:35 +0700

    add order mapping

---
+++ Repository/Configuration/Mappings/OrderMapConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using BusinessObject;
+using Mapster;
+using Repository.Models.Orders;
+
+namespace Repository.Configuration.Mappings
+{
+    public static class OrderMapConfiguration
+    {
+        public static void RegisterOrderMapping()
+        {
+            TypeAdapterConfig<Order, InfoOrder>
+                .NewConfig()
+                .Map(dest => dest.Products, src => src.OrderDetails!.Count);
+        }
+    }
+}
