4950586ad212b4d8f178862705bb2968a28141ad
ngianguyen81 <ngianguyen81@gmail.com>
2022-11-26 16:48:27 +0700

    grocery exclude products from inactive user

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -3,6 +3,7 @@ using Mapster;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Repository.Constants.Products;
+using Repository.Constants.Users;
 using Repository.Interfaces;
 using Repository.Models.Product;
 
@@ -212,18 +213,20 @@ namespace Repository
             var result = new List<GroceryProduct>();
             var db = new CakeCuriousDbContext();
             string query = (storeId != null)
-                ? $"select top {take} [p].[id], [p].[product_type], [p].[name], [p].[price], [p].[discount], [p].[photo_url], abs(checksum([p].id, rand(@randSeed)*rand(@randSeed))) as [key] from [Product] as [p] left join [Store] as [s] on [p].[store_id] = [s].[id] where abs(checksum([p].id, rand(@randSeed)* rand(@randSeed))) > @key and ([p].[product_type] = @productType) and ([p].[status] = @productStatus ) and ([s].[status] = @storeStatus ) and ([s].[id] = @storeId) order by abs(checksum([p].id, rand(@randSeed) * rand(@randSeed)))"
-                : $"select top {take} [p].[id], [p].[product_type], [p].[name], [p].[price], [p].[discount], [p].[photo_url], abs(checksum([p].id, rand(@randSeed)*rand(@randSeed))) as [key] from [Product] as [p] left join [Store] as [s] on [p].[store_id] = [s].[id] where abs(checksum([p].id, rand(@randSeed)* rand(@randSeed))) > @key and ([p].[product_type] = @productType) and ([p].[status] = @productStatus ) and ([s].[status] = @storeStatus ) order by abs(checksum([p].id, rand(@randSeed) * rand(@randSeed)))";
+                ? $"select top {take} [p].[id], [p].[product_type], [p].[name], [p].[price], [p].[discount], [p].[photo_url], abs(checksum([p].id, rand(@randSeed)*rand(@randSeed))) as [key] from [Product] as [p] left join [Store] as [s] on [p].[store_id] = [s].[id] left join [User] as [u] on [s].[user_id] = [u].[id] where abs(checksum([p].id, rand(@randSeed)* rand(@randSeed))) > @key and ([p].[product_type] = @productType) and ([p].[status] = @productStatus) and ([s].[status] = @storeStatus) and ([u].[status] = @userStatus) and ([s].[id] = @storeId) order by abs(checksum([p].id, rand(@randSeed) * rand(@randSeed)))"
+                : $"select top {take} [p].[id], [p].[product_type], [p].[name], [p].[price], [p].[discount], [p].[photo_url], abs(checksum([p].id, rand(@randSeed)*rand(@randSeed))) as [key] from [Product] as [p] left join [Store] as [s] on [p].[store_id] = [s].[id] where abs(checksum([p].id, rand(@randSeed)* rand(@randSeed))) > @key and ([p].[product_type] = @productType) and ([p].[status] = @productStatus) and ([s].[status] = @storeStatus) and ([u].[status] = @userStatus) order by abs(checksum([p].id, rand(@randSeed) * rand(@randSeed)))";
             var cmd = db.Database.GetDbConnection().CreateCommand();
             cmd.CommandText = query;
             var productStatus = (int)ProductStatusEnum.Active;
             var storeStatus = (int)StoreStatusEnum.Active;
+            var userStatus = (int)UserStatusEnum.Active;
             cmd.Parameters.Add(new SqlParameter("@take", take));
             cmd.Parameters.Add(new SqlParameter("@randSeed", randSeed));
             cmd.Parameters.Add(new SqlParameter("@key", key));
             cmd.Parameters.Add(new SqlParameter("@productType", productType));
             cmd.Parameters.Add(new SqlParameter("@productStatus", productStatus));
             cmd.Parameters.Add(new SqlParameter("@storeStatus", storeStatus));
+            cmd.Parameters.Add(new SqlParameter("@userStatus", userStatus));
             if (storeId != null)
             {
                 cmd.Parameters.Add(new SqlParameter("@storeId", storeId));
