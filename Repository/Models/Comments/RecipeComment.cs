4cd9d96bc08b09cd9e52601c53d4478d4782f6e7
ngianguyen81 <ngianguyen81@gmail.com>
2022-11-30 21:03:55 +0700

    add rootId to comment

--- Repository/Models/Comments/RecipeComment.cs
+++ Repository/Models/Comments/RecipeComment.cs
@@ -8,6 +8,7 @@ namespace Repository.Models.Comments
         public SimpleUser? User { get; set; }
         public string? Content { get; set; }
         public DateTime? SubmittedDate { get; set; }
+        public Guid? RootId { get; set; }
         public int? Depth { get; set; }
         public IEnumerable<RecipeCommentMedia>? Images { get; set; }
         public int? RepliesCount { get; set; }
