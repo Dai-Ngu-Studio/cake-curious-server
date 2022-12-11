a9e8a35a69265d601b36a8ed19cbe71bc8870811
ngianguyen81 <ngianguyen81@gmail.com>
2022-12-11 13:54:02 +0700

    bsn. models for reason

---
+++ Repository/Models/DeactivateReasons/CreateDeactivateReason.cs
@@ -0,0 +1,10 @@
+﻿namespace Repository.Models.DeactivateReasons
+{
+    public class CreateDeactivateReason
+    {
+        public Guid? ItemId { get; set; }
+        public int? ItemType { get; set; }
+        public string? Reason { get; set; }
+        public string? UserId { get; set; }
+    }
+}
---
+++ Repository/Models/DeactivateReasons/DetailDeactivateReason.cs
@@ -0,0 +1,15 @@
+﻿using Repository.Models.Users;
+
+namespace Repository.Models.DeactivateReasons
+{
+    public class DetailDeactivateReason
+    {
+        public Guid? Id { get; set; }
+        public Guid? ItemId { get; set; }
+        public int? ItemType { get; set; }
+        public string? StaffId { get; set; }
+        public SimpleUser? Staff { get; set; }
+        public string? Reason { get; set; }
+        public DateTime? DeactivateDate { get; set; }
+    }
+}
