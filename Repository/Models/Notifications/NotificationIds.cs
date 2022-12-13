d53e2016df46c018197e075c264340ac61b1565c
ngianguyen81 <ngianguyen81@gmail.com>
2022-12-13 23:36:14 +0700

    add bsn. models for notification

---
+++ Repository/Models/Notifications/NotificationIds.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Repository.Models.Notifications
+{
+    public class NotificationIds
+    {
+        [Required]
+        public IEnumerable<Guid>? Ids { get; set; }
+    }
+}
---
+++ Repository/Models/Notifications/PushNotification.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Repository.Models.Notifications
+{
+    public class PushNotification
+    {
+        [Required]
+        public string? ReceiverId { get; set; }
+
+        [Required]
+        public int? ItemType { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Content { get; set; }
+    }
+}
