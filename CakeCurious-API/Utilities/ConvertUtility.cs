fbc435ce0814a078560d9de9a445ddc2cc8302a6
ngianguyen81 <ngianguyen81@gmail.com>
2022-12-14 13:44:08 +0700

    add shortguid converter

--- CakeCurious-API/Utilities/ConvertUtility.cs
+++ CakeCurious-API/Utilities/ConvertUtility.cs
@@ -11,5 +11,20 @@ namespace CakeCurious_API.Utilities
             byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(value));
             return new Guid(data);
         }
+
+        public static string ToShortGuid(this Guid newGuid)
+        {
+            string modifiedBase64 = Convert.ToBase64String(newGuid.ToByteArray())
+                .Replace('+', '-').Replace('/', '_') // avoid invalid URL characters
+                .Substring(0, 22);
+            return modifiedBase64;
+        }
+
+        public static Guid ParseShortGuid(string shortGuid)
+        {
+            string base64 = shortGuid.Replace('-', '+').Replace('_', '/') + "==";
+            Byte[] bytes = Convert.FromBase64String(base64);
+            return new Guid(bytes);
+        }
     }
 }
