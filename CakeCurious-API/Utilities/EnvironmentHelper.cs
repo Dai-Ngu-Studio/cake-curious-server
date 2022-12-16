1c098c3262c93ab114e25a9d0e00a7c1d6679184
ngianguyen81 <ngianguyen81@gmail.com>
2022-12-16 20:48:33 +0700

    update appsettings, docker-compose

--- CakeCurious-API/Utilities/EnvironmentHelper.cs
+++ CakeCurious-API/Utilities/EnvironmentHelper.cs
@@ -12,6 +12,10 @@
         public static readonly string ShareImageHeight = "SHARE_IMAGE_HEIGHT";
         public static readonly string InputGMT = "INPUT_GMT";
         public static readonly string DailyScheduledTime = "DAILY_SCHEDULED_TIME";
+        public static readonly string SmtpSenderMailAddress = "SMTP_SENDER_MAIL_ADDRESS";
+        public static readonly string SmtpSenderName = "SMTP_SENDER_NAME";
+        public static readonly string NewStaffMailSubject = "NEW_STAFF_MAIL_SUBJECT";
+        public static readonly string NewStaffMailBody = "NEW_STAFF_MAIL_BODY";
 
         public static void AddAppInfoEnvironmentVariables(IConfigurationSection appInfo)
         {
@@ -23,6 +27,10 @@
             Environment.SetEnvironmentVariable(SuffixOption, appInfo.GetValue<string>("SuffixOption"));
             Environment.SetEnvironmentVariable(ShareImageWidth, appInfo.GetValue<string>("ShareImageWidth"));
             Environment.SetEnvironmentVariable(ShareImageHeight, appInfo.GetValue<string>("ShareImageHeight"));
+            Environment.SetEnvironmentVariable(SmtpSenderMailAddress, appInfo.GetValue<string>("SmtpSenderMailAddress"));
+            Environment.SetEnvironmentVariable(SmtpSenderName, appInfo.GetValue<string>("SmtpSenderName"));
+            Environment.SetEnvironmentVariable(NewStaffMailSubject, appInfo.GetValue<string>("NewStaffMailSubject"));
+            Environment.SetEnvironmentVariable(NewStaffMailBody, appInfo.GetValue<string>("NewStaffMailBody"));
         }
 
         public static void AddServiceConfigurationEnvironmentVariables(IConfigurationSection serviceConfiguration)
